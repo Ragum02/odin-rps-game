83857ef541e28ed0b321df8c1b40254f6d9c27cf
Ragum02 <benjamin.souiket@gmail.com>
2024-10-11 09:05:15 +0200

    new Playround function + score

--- javascript.js
+++ javascript.js
@@ -6,8 +6,9 @@ console.log("Hello World");
 // compare both input
 
 const randomizeChoice = Math.floor(Math.random() * 3) + 1;
-const userInput = prompt("Rock,Paper,Scissors?: ")
-
+const userInput = prompt("Rock,Paper,Scissors?: ").toLowerCase();
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice(number){
     if(number === 1){
@@ -20,15 +21,31 @@ function getComputerChoice(number){
 }
 
 function getHumanChoice(string){
-    if(string === "Rock"){
+    if(string === "rock"){
         return "Rock"
-    } else if (string === "Paper") {
+    } else if (string === "paper") {
         return "Paper";
-    } else if (string === "Scissors"){
+    } else if (string === "scissors"){
         return "Scissors";
     } else {
         alert("Invalid answer");
     }
 }
+
+function playRound(humanChoice, computerChoice){
+    if(humanChoice < computerChoice){
+console.log(`${humanChoice} beat ${computerChoice}`);
+humanScore++;
+console.log("You have: " + humanScore + " points");
+    } else if (humanChoice > computerChoice){
+        console.log(`${computerChoice} beat ${humanChoice}`);
+        computerScore++;
+console.log("Computer has: " + computerScore + " points");
+    } else{
+        console.log("Draw !");
+    }
+}
+
+playRound(getHumanChoice(userInput),getComputerChoice(randomizeChoice));
 console.log(getComputerChoice(randomizeChoice));
-console.log(getHumanChoice(userInput.value))
+console.log(getHumanChoice(userInput))
