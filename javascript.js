7a94d93a65adba2ac1918fb026f143770bd6b0aa
Ragum02 <benjamin.souiket@gmail.com>
2024-10-11 09:29:24 +0200

    create round variable , move computer & user variable to new playGame function.

--- javascript.js
+++ javascript.js
@@ -5,10 +5,10 @@ console.log("Hello World");
 // Randomize computer input
 // compare both input
 
-const randomizeChoice = Math.floor(Math.random() * 3) + 1;
-const userInput = prompt("Rock,Paper,Scissors?: ").toLowerCase();
+
 let humanScore = 0;
 let computerScore = 0;
+let round = 1;
 
 function getComputerChoice(number){
     if(number === 1){
@@ -34,18 +34,33 @@ function getHumanChoice(string){
 
 function playRound(humanChoice, computerChoice){
     if(humanChoice < computerChoice){
-console.log(`${humanChoice} beat ${computerChoice}`);
+console.log(`You win this round ! ${humanChoice} beat ${computerChoice}`);
 humanScore++;
 console.log("You have: " + humanScore + " points");
     } else if (humanChoice > computerChoice){
-        console.log(`${computerChoice} beat ${humanChoice}`);
+        console.log(`You lose this round ! ${humanChoice} lose against ${computerChoice} `);
         computerScore++;
 console.log("Computer has: " + computerScore + " points");
-    } else{
-        console.log("Draw !");
+    } else if (humanChoice === computerChoice){
+        console.log("It's a Draw for this round!");
+    } else {
+        console.log("Invalid user answer !");
     }
 }
 
+function playGame(round){
+   while(round <= 5){
+    const randomizeChoice = Math.floor(Math.random() * 3) + 1;
+    const userInput = prompt("Rock,Paper,Scissors?: ").toLowerCase();
 playRound(getHumanChoice(userInput),getComputerChoice(randomizeChoice));
-console.log(getComputerChoice(randomizeChoice));
-console.log(getHumanChoice(userInput))
+    round++;
+} if(humanScore > computerScore){
+console.log (`You win the game!  You: ${humanScore} Computer: ${computerScore}`);
+} else if (humanScore < computerScore) {
+    console.log(`You lose the game! You: ${humanScore} Computer: ${computerScore}`);
+} else {
+    console.log("DRAAAW !");
+}
+}
+
+playGame(round);
