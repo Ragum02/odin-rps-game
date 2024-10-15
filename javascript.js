fc2cf6c60164d083972f4dd9a85a5906a227f769
Ragum02 <benjamin.souiket@gmail.com>
2024-10-15 21:53:58 +0200

    Making some simple "ui".

--- javascript.js
+++ javascript.js
@@ -8,19 +8,52 @@ console.log("Hello World");
 
 let humanScore = 0;
 let computerScore = 0;
-let round = 1;
+let round = 0;
+const result = document.querySelector("#display")
+const score = document.querySelector("#score");
+const selectButtonList = document.querySelector("#select")
+
+
 
 function getComputerChoice(number){
     if(number === 1){
-        return "Rock";
+        return "rock";
     } else if (number === 2) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 }
 
-function getHumanChoice(string){
+selectButtonList.addEventListener("click", function getHumanChoice(event) {
+result.textContent = "";
+ if(round === 5){
+    if (humanScore > computerScore) {
+        const finalWin = document.createElement("span");
+        finalWin.textContent = `You win the game! You: ${humanScore} Computer: ${computerScore}`;
+        result.appendChild(finalWin);
+    } else if (humanScore < computerScore) {
+        const finalLose = document.createElement("span");
+        finalLose.textContent = `You lose the game! You: ${humanScore} Computer: ${computerScore}`;
+        result.appendChild(finalLose);
+    } else {
+        const finalTie = document.createElement("span");
+        finalTie.textContent = "It's a tie!";
+        result.appendChild(finalTie);
+    }
+} else {
+    round++
+    let playerSelection = event.target.id;
+    if(playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors"){
+        const randomizeChoice = Math.floor(Math.random() * 3) + 1;
+        const computerSelection = getComputerChoice(randomizeChoice);
+        playRound(playerSelection, computerSelection);
+    }
+}})
+;
+
+
+/*function getHumanChoice(string){
     if(string === "rock"){
         return "Rock"
     } else if (string === "paper") {
@@ -30,37 +63,24 @@ function getHumanChoice(string){
     } else {
         alert("Invalid answer");
     }
-}
+}*/
 
-function playRound(humanChoice, computerChoice){
-    if(humanChoice < computerChoice){
-console.log(`You win this round ! ${humanChoice} beat ${computerChoice}`);
-humanScore++;
-console.log("You have: " + humanScore + " points");
-    } else if (humanChoice > computerChoice){
-        console.log(`You lose this round ! ${humanChoice} lose against ${computerChoice} `);
-        computerScore++;
-console.log("Computer has: " + computerScore + " points");
-    } else if (humanChoice === computerChoice){
-        console.log("It's a tie for this round!");
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        const tieMsg = document.createElement("span");
+        tieMsg.textContent = "It's a tie for this round!";
+        result.appendChild(tieMsg);
+    } else if ( (humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        const winMsg = document.createElement("span");
+        winMsg.textContent = `You win this round! ${humanChoice} beats ${computerChoice}`;
+        result.appendChild(winMsg);
+        humanScore++;
     } else {
-        console.log("Invalid user answer !");
+        const loseMsg = document.createElement("span");
+        loseMsg.textContent = `You lose this round! ${computerChoice} beats ${humanChoice}`;
+        result.appendChild(loseMsg);
+        computerScore++;
     }
 }
 
-function playGame(round){
-   while(round <= 5){
-    const randomizeChoice = Math.floor(Math.random() * 3) + 1;
-    const userInput = prompt("Rock,Paper,Scissors?: ").toLowerCase();
-playRound(getHumanChoice(userInput),getComputerChoice(randomizeChoice));
-    round++;
-} if(humanScore > computerScore){
-console.log (`You win the game!  You: ${humanScore} Computer: ${computerScore}`);
-} else if (humanScore < computerScore) {
-    console.log(`You lose the game! You: ${humanScore} Computer: ${computerScore}`);
-} else {
-    console.log("TIIIIEE ! ");
-}
-}
-
-playGame(round);
